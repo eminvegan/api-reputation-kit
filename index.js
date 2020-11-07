ffa91b1f07b4f8ec46f66808261c68b890b59bc8
eminvegan <eminvegan@gmail.com>
2020-11-08 00:51:15 +0100

    Initialization 03

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import routes from './src/routes/googleRoutes';
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
 const app = express();
