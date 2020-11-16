09e2dbe99bac16b966f5b209c802168d4bece1fd
eminvegan <eminvegan@gmail.com>
2020-11-16 10:36:37 +0100

    headers

--- src/routes/googleRoutes.js
+++ src/routes/googleRoutes.js
@@ -1,27 +1,31 @@
-import { getAmount, getFirstPage, getAll } from '../controllers/googleController';
+import {
+	getAmount,
+	getFirstPage,
+	getAll
+} from '../controllers/googleController';
 
 const routes = (app) => {
 
-  app.get('/google/:placeID/all', async (req, res) => {
-      res.setHeader('Connection', 'keep-alive');
-      res.setHeader('Cache-Control', 'no-cache');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      await getAll(req, res);
-  });
+	app.get('/google/:placeID/all', async (req, res) => {
+		// res.setHeader('Connection', 'keep-alive');
+		// res.setHeader('Cache-Control', 'no-cache');
+		// res.setHeader('Access-Control-Allow-Origin', '*');
+		await getAll(req, res);
+	});
 
-  app.get('/google/:placeID/page', async (req, res) => {
-      res.setHeader('Connection', 'keep-alive');
-      res.setHeader('Cache-Control', 'no-cache');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      await getFirstPage(req, res);
-  });
+	app.get('/google/:placeID/page', async (req, res) => {
+		// res.setHeader('Connection', 'keep-alive');
+		// res.setHeader('Cache-Control', 'no-cache');
+		// res.setHeader('Access-Control-Allow-Origin', '*');
+		await getFirstPage(req, res);
+	});
 
-  app.get('/google/:placeID/amount', async (req, res) => {
-      res.setHeader('Connection', 'keep-alive');
-      res.setHeader('Cache-Control', 'no-cache');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      await getAmount(req, res);
-  });
+	app.get('/google/:placeID/amount', async (req, res) => {
+		// res.setHeader('Connection', 'keep-alive');
+		// res.setHeader('Cache-Control', 'no-cache');
+		// res.setHeader('Access-Control-Allow-Origin', '*');
+		await getAmount(req, res);
+	});
 
 };
 
