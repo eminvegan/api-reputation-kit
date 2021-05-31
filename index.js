c086f9c0ac74da62220eacead5ba3fdf8fbd5e5c
eminvegan <eminvegan@gmail.com>
2021-05-31 10:44:29 +0200

    .

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import routes from './src/routes/googleRoutes';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(cors);
 
 routes(app);
 
