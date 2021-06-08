420fc172e5e08df4c4c7edcc05c0b4d2eade11a0
eminvegan <eminvegan@gmail.com>
2021-06-08 17:57:23 +0200

    .

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ import cors from 'cors';
 const app = express();
 const PORT = 3000;
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:4200',
-  'http://127.0.0.1:8000',
-  'https://assessmentprofessional.etechnocrat.tech',
-  'https://dein-ruf.de',
-  'https://dein-bewertungsprofi.de',
-  'https://dein-bewertungsprofi.de/',
-  'https://dein-bewertungsprofi.de/review-reporter',
-  'https://dein-bewertungsprofi.de/review-reporter/',
-  'https://dein-bewertungsprofi.local:8890',
-  'https://dein-bewertungsprofi.local:8890/',
-  'https://dein-bewertungsprofi.local:8890/review-reporter',
-  'https://dein-bewertungsprofi.local:8890/review-reporter/',
-];
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   'http://localhost:4200',
+//   'http://127.0.0.1:8000',
+//   'https://assessmentprofessional.etechnocrat.tech',
+//   'https://dein-ruf.de',
+//   'https://dein-bewertungsprofi.de',
+//   'https://dein-bewertungsprofi.de/',
+//   'https://dein-bewertungsprofi.de/review-reporter',
+//   'https://dein-bewertungsprofi.de/review-reporter/',
+//   'https://dein-bewertungsprofi.local:8890',
+//   'https://dein-bewertungsprofi.local:8890/',
+//   'https://dein-bewertungsprofi.local:8890/review-reporter',
+//   'https://dein-bewertungsprofi.local:8890/review-reporter/',
+// ];
 
-const options = {
-  origin: '*',
-  optionsSuccessStatus: 200,
-};
+// const options = {
+//   origin: '*',
+//   optionsSuccessStatus: 200,
+// };
 
-app.use(cors(options));
+// app.use(cors(options));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
